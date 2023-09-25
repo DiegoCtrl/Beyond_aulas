31545f090afb5554557714eeb4380b9d23083646
Diego Araújo <118080825+DiegoCtrl@users.noreply.github.com>
2023-09-25 12:19:57 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="https://avatars.githubusercontent.com/u/118080825?v=4" class="logo perfil"  alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Olá Mundo" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #ffffffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.perfil{
+  border-radius: 50%;
+}
+
+.fundo{
+  background-color: rgb(44, 40, 40);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class = "corfonte">{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">contador de clicks {{ count }}</button>
+    <p>
+      Edit
+      <code class = "corfonte">Um novo patamar</code> A Era Vue
+    </p>
+  </div>
+
+ 
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.corfonte{
+  color: azure;
+}
+</style>
